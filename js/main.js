88b99f2e98958159f02b312d2a94caaa6f9783cf
Yuriy Zudikhin <uzudihin@gmail.com>
2020-03-28 13:56:24 +0300

    first-commit

---
+++ js/main.js
@@ -0,0 +1,111 @@
+$(document).ready(function() {
+  "use strict";
+  $(".header__desc-btn").click(function() {
+    $(this).toggleClass("active");
+    $(".header__dropdown").toggleClass("active");
+  });
+
+  $(".header__mobile__btn").click(function() {
+    $(this).toggleClass("active");
+    $(".header__mobile__nav").toggleClass("active");
+  });
+
+  $(".sertificate__slider").slick({
+    prevArrow: $(".sertificate__prev"),
+    nextArrow: $(".sertificate__next"),
+    responsive: [
+      {
+        breakpoint: 7200,
+        settings: {
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 2
+        }
+      }
+    ]
+  });
+
+  $(".reviews__slider").slick({
+    prevArrow: $(".reviews__prev"),
+    nextArrow: $(".reviews__next")
+  });
+
+  $(".main__item").each(function() {
+    if ($(this).attr("data-bg")) {
+      $(this).css({
+        background: "url(" + $(this).data("bg") + ")",
+        "background-position": "center center",
+        "background-repeat": "no-repeat",
+        "background-size": "cover"
+      });
+    }
+  });
+
+  $(".main__slider").slick({
+    infinite: true,
+    speed: 350,
+    autoplay: false,
+    autoplaySpeed: 2000,
+    dots: true,
+    customPaging: function(slider, i) {
+      return '<a class="dot"></a>';
+    }
+  });
+  function bar() {
+    var i = 0;
+    if (i == 0) {
+      i = 1;
+      var width = 1;
+      var id = setInterval(frame, 10);
+      function frame() {
+        if (width >= 100) {
+          clearInterval(id);
+          i = 0;
+        } else {
+          width++;
+          $(".shop__bug").css({ width: width + "%" });
+        }
+      }
+    }
+  }
+  bar();
+
+  $(".shop__img").hover(function() {
+    $(this)
+      .children()
+      .eq(1)
+      .toggleClass("active");
+  });
+
+  $(".shop__btn-desc").click(function() {
+    $(this).addClass("loading");
+  });
+
+  $(".shop__btn-mob").click(function() {
+    $(this).addClass("loading");
+  });
+
+  $(".sertificate__item").hover(function() {
+    $(this).toggleClass("active");
+  });
+
+  $(".cart__fake-btn").click(function() {
+    $(this).css("display", "none");
+    $(".cart__contact").addClass("active");
+    $(".cart__delivery").addClass("active");
+    $("#delivery").css("display", "block");
+    $(".cart__payment").addClass("active");
+    $(".cart__submit").addClass("active");
+  });
+});
