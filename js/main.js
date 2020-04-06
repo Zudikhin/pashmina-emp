59221a8ca82fd826362204d7939d82f63250e0ad
Yuriy Zudikhin <uzudihin@gmail.com>
2020-04-06 12:28:00 +0300

    accaount

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-$(document).ready(function() {
+$(document).ready(function () {
   "use strict";
-  $(".header__desc-btn").click(function() {
+  $(".header__desc-btn").click(function () {
     $(this).toggleClass("active");
     $(".header__dropdown").toggleClass("active");
   });
 
-  $(".header__mobile__btn").click(function() {
+  $(".header__mobile__btn").click(function () {
     $(this).toggleClass("active");
     $(".header__mobile__nav").toggleClass("active");
   });
@@ -17,29 +17,29 @@ $(document).ready(function() {
       {
         breakpoint: 7200,
         settings: {
-          slidesToShow: 4
-        }
+          slidesToShow: 4,
+        },
       },
       {
         breakpoint: 1200,
         settings: {
-          slidesToShow: 3
-        }
+          slidesToShow: 3,
+        },
       },
       {
         breakpoint: 992,
         settings: {
-          slidesToShow: 2
-        }
+          slidesToShow: 2,
+        },
       },
       {
         breakpoint: 768,
         settings: {
           slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
+          slidesToScroll: 1,
+        },
+      },
+    ],
   });
 
   $(".product__images .slider").slick({
@@ -49,29 +49,29 @@ $(document).ready(function() {
       {
         breakpoint: 7200,
         settings: {
-          slidesToShow: 3
-        }
+          slidesToShow: 3,
+        },
       },
       {
         breakpoint: 1200,
         settings: {
-          slidesToShow: 2
-        }
+          slidesToShow: 2,
+        },
       },
       {
         breakpoint: 992,
         settings: {
-          slidesToShow: 2
-        }
+          slidesToShow: 2,
+        },
       },
       {
         breakpoint: 768,
         settings: {
           slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
+          slidesToScroll: 1,
+        },
+      },
+    ],
   });
 
   $(".sertificate__slider").slick({
@@ -81,38 +81,38 @@ $(document).ready(function() {
       {
         breakpoint: 7200,
         settings: {
-          slidesToShow: 3
-        }
+          slidesToShow: 3,
+        },
       },
       {
         breakpoint: 992,
         settings: {
-          slidesToShow: 2
-        }
+          slidesToShow: 2,
+        },
       },
       {
         breakpoint: 768,
         settings: {
           slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
+          slidesToScroll: 1,
+        },
+      },
+    ],
   });
 
   $(".reviews__slider").slick({
     prevArrow: $(".reviews__prev"),
-    nextArrow: $(".reviews__next")
+    nextArrow: $(".reviews__next"),
   });
 
-  $(".main__item").each(function() {
+  $(".main__item").each(function () {
     if (window.matchMedia("(min-width: 576px)").matches) {
       if ($(this).attr("data-bg")) {
         $(this).css({
           background: "url(" + $(this).data("bg") + ")",
           "background-position": "50% 0%",
           "background-repeat": "no-repeat",
-          "background-size": "cover"
+          "background-size": "cover",
         });
       }
     } else {
@@ -121,7 +121,7 @@ $(document).ready(function() {
           background: "url(" + $(this).data("bg") + ")",
           "background-position": "90% 0%",
           "background-repeat": "no-repeat",
-          "background-size": "cover"
+          "background-size": "cover",
         });
       }
     }
@@ -133,9 +133,9 @@ $(document).ready(function() {
     autoplay: false,
     autoplaySpeed: 2000,
     dots: true,
-    customPaging: function(slider, i) {
+    customPaging: function (slider, i) {
       return '<a class="dot"></a>';
-    }
+    },
   });
 
   function bar() {
@@ -157,38 +157,33 @@ $(document).ready(function() {
   }
   bar();
 
-  $(".shop__img").hover(function() {
-    $(this)
-      .children()
-      .eq(1)
-      .toggleClass("active");
+  $(".shop__img").hover(function () {
+    $(this).children().eq(1).toggleClass("active");
   });
 
-  $(".shop__btn-desc").click(function() {
+  $(".shop__btn-desc").click(function () {
     $(this).addClass("loading");
   });
 
-  $(".shop__btn-mob").click(function() {
+  $(".shop__btn-mob").click(function () {
     $(this).addClass("loading");
   });
 
-  $(".sertificate__item").hover(function() {
+  $(".sertificate__item").hover(function () {
     $(this).toggleClass("active");
   });
 
-  $(".product__description .drop .item h3").click(function() {
-    $(this)
-      .parent()
-      .toggleClass("active");
+  $(".product__description .drop .item h3").click(function () {
+    $(this).parent().toggleClass("active");
   });
 
   var $videoSrc;
-  $(".video-btn").click(function() {
+  $(".video-btn").click(function () {
     $videoSrc = $(this).data("src");
   });
 
   // when the modal is opened autoplay it
-  $("#myModal").on("shown.bs.modal", function(e) {
+  $("#myModal").on("shown.bs.modal", function (e) {
     // set the video src to autoplay and not to show related video. Youtube related video is like a box of chocolates... you never know what you're gonna get
     $("#video").attr(
       "src",
@@ -197,8 +192,19 @@ $(document).ready(function() {
   });
 
   // stop playing the youtube video when I close the modal
-  $("#myModal").on("hide.bs.modal", function(e) {
+  $("#myModal").on("hide.bs.modal", function (e) {
     // a poor man's stop video
     $("#video").attr("src", $videoSrc);
+  });
+  $("#order").click(function (event) {
+    event.preventDefault();
+    $(".account__content .order").addClass("active");
+    $(".account__content .personal").addClass("active");
+  });
+
+  $("#lk").click(function (event) {
+    event.preventDefault();
+    $(".account__content .order").removeClass("active");
+    $(".account__content .personal").removeClass("active");
   });
 });
